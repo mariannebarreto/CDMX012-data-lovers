97bb3da92fd3aa77395c7a4899e3580169d00f14
EliTejeda <tejeda.eliza@gmail.com>
2022-02-17 12:00:28 -0600

    Avance seccion Male en mainjs y datajs

--- src/data.js
+++ src/data.js
@@ -10,6 +10,19 @@ export const anotherExample = () => {
 
 /*aquí comienza mi prueba-eli*/
 export const alphabeticOrder = (resultsData) => resultsData.sort((character1, character2) => {
-  return ( character1.name < character2.name )?-1:1 //algoritmo de ordenamiento
+  return (character1.name < character2.name) ? -1 : 1; //algoritmo de ordenamiento
 
- }) 
+});
+/* aqui funcion para jalar la data por Male-eli*/
+export const maleSeccOrder = (resultsData) => {
+  const resultsMale= [ ]
+  for (let i = 0; i < resultsData.length; i++) {   //recorrer lista de datos
+    if (resultsData[i].gender == "Male") {//evaluando parametro resultsdata y para indicar la posicion detro del array , gender es lo que vamos a comparar
+      /*console.log(resultsData[i].name);*/
+      resultsMale.push(resultsData[i]); //variable donde se guardan los resultados de male
+    } 
+  
+  }
+  return resultsMale;
+}
+ 
--- src/main.js
+++ src/main.js
@@ -5,14 +5,35 @@ import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
 
-/* aqui empieza mi prueba -eli*/
-import { alphabeticOrder } from './data.js';
+/* funcion jalar seccion Male*/
+import { alphabeticOrder, maleSeccOrder } from './data.js';
 
 let resultsData = data.results;
-/*console.log(resultsData[1].name)*/
+console.log(resultsData[1].name)
 alphabeticOrder ( resultsData )
 for (let i = 0; i <= 10; i++){ //ciclo
 
     console.log(resultsData [i].name)
 }
+/* guardar variable de Male*/
+maleSeccOrder(resultsData);//llamar a la funcion
+
+const resultsMale = [];
+
+resultsMale = maleSeccOrder(resultsData);//resultados se guardan en la variable
+
+
+/*datos dentro de la cuadro de Male*/
+/*function addCard (name, status, species, type, gender, origin, location, imagen, episode) {
+    
+    let card = document.getElementById("tarjeta")
+    let newCard = card.cloneNode(true);
+
+    newCard.style.display = "inline - block";
+    newCard.style.margin = "1em";
+    
+    let nameCharacter = document.getElementById('nameCharacter'); 
+    nameCharacter*/
+
+
 
