276b05f2dfbcd80999d14ad29ae1b8600189f655
mariannebarreto <barretoink@icloud.com>
2022-02-17 19:07:13 -0600

    correccion women

--- src/data.js
+++ src/data.js
@@ -1,28 +1,24 @@
-// estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
-};
-
-/*aquí comienza mi prueba-eli*/
+//declaramos funciones para cada sección 
+//ordenado alfabético
 export const alphabeticOrder = (resultsData) => resultsData.sort((character1, character2) => {
   return (character1.name < character2.name) ? -1 : 1; //algoritmo de ordenamiento
 
 });
-/* aqui funcion para jalar la data por Male-eli*/
-export const maleSeccOrder = (resultsData) => {
-  const resultsMale= [ ]
+
+//--------------------- WOMEN------------
+
+//función para obtener data para la sección
+export const femaleSeccOrder = (resultsData) => {
+  const resultsFemale= [ ]
   for (let i = 0; i < resultsData.length; i++) {   //recorrer lista de datos
-    if (resultsData[i].gender == "Male") {//evaluando parametro resultsdata y para indicar la posicion detro del array , gender es lo que vamos a comparar
+    if (resultsData[i].gender == "Female") {//evaluando parametro resultsdata y para indicar la posicion detro del array , gender es lo que vamos a comparar
       /*console.log(resultsData[i].name);*/
-      resultsMale.push(resultsData[i]); //variable donde se guardan los resultados de male
+      resultsFemale.push(resultsData[i]); 
     } 
   
   }
-  return resultsMale;
+  return resultsFemale;
 }
- 
+export const femaleInfo = (results) => {
+  return ( results.filter(element => element.gender == "Female")) 
+}
--- src/main.js
+++ src/main.js
@@ -1,15 +1,34 @@
+//declaramos de donde obtendremos nuestra data
 import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+//------------WOMEN|| FEMALE DATA--------------
 
-/* aqui empieza mi prueba -eli*/
-import { alphabeticOrder } from './data.js';
-
+// importamos funciones para obtener data FEMALE
+import { alphabeticOrder, femaleSeccOrder, femaleInfo } from './data.js';
+//declaramos variables
 let resultsData = data.results;
-/*console.log(resultsData[1].name)*/
 alphabeticOrder ( resultsData )
 for (let i = 0; i <= 10; i++){ //ciclo
 
-    console.log(resultsData [i].name)
 }
+//guarda variable de FEMALE
+femaleSeccOrder(resultsData);//llamar a la funcion
+
+ /*resultsFemale = maleSeccOrder(resultsData);//resultados se guardan en la variable*/
+
+let femaleData = document.getElementById("women");
+let femaleHtml = ``
+let femaleFilter = femaleInfo(resultsData);
+
+/*maleFilter.forEach(girls=>{
+    
+    maleHtml += '<h3>${girls.name}</h3>'
+    
+});*/
+femaleFilter.forEach(women=>{
+    femaleHtml+=`<article><h3>${women.name}</h3></article>`
+    
+})
+femaleData.innerHTML = femaleHtml;
+
 
