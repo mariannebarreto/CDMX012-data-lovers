066a21e1a166698534799010749589d487e2da48
mariannebarreto <barretoink@icloud.com>
2022-02-17 21:14:05 -0600

    cierre día: a- z

--- src/data.js
+++ src/data.js
@@ -1,24 +1,43 @@
-//declaramos funciones para cada sección 
+//****************DECLARAMOS FUNCIONES PARA CADA SECCION ******/
+
 //ordenado alfabético
 export const alphabeticOrder = (resultsData) => resultsData.sort((character1, character2) => {
   return (character1.name < character2.name) ? -1 : 1; //algoritmo de ordenamiento
-
 });
 
-//--------------------- WOMEN------------
+export const alphabeticOrderBack = (resultsData) => resultsData.sort((character1, character2) => {
+  return (character1.name > character2.name) ? -1 : 1;
+});
 
-//función para obtener data para la sección
-export const femaleSeccOrder = (resultsData) => {
-  const resultsFemale= [ ]
-  for (let i = 0; i < resultsData.length; i++) {   //recorrer lista de datos
-    if (resultsData[i].gender == "Female") {//evaluando parametro resultsdata y para indicar la posicion detro del array , gender es lo que vamos a comparar
-      /*console.log(resultsData[i].name);*/
-      resultsFemale.push(resultsData[i]); 
-    } 
+// status personaje
+  export const characterStatus = (data) => {
+    let alive = 0;
+    let dead = 0;
+    let unknown = 0;
   
+    data.forEach((character) => {
+    switch (character.status) {
+      case 'Alive':
+        alive += 1;
+      break;
+    case 'Dead':
+        dead += 1;
+      break;
+    case 'unknown':
+        unknown += 1;
   }
-  return resultsFemale;
-}
-export const femaleInfo = (results) => {
-  return ( results.filter(element => element.gender == "Female")) 
-}
+  });
+  return [ alive, dead, unknown ]
+  }
+
+
+
+
+
+
+
+
+
+
+
+
