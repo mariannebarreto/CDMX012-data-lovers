2506a0c79e224923e363dbd01f5d0869ae17f63b
EliTejeda <tejeda.eliza@gmail.com>
2022-02-21 10:34:16 -0600

    Avances domingo20feb

--- src/data.js
+++ src/data.js
@@ -3,29 +3,84 @@
 export const example = () => {
   return 'example';
 };
+//****************DECLARAMOS FUNCIONES PARA LAS SECCIONES******/
 
-export const anotherExample = () => {
-  return 'OMG';
-};
-
-/*aquí comienza mi prueba-eli*/
+//ordenado alfabético
 export const alphabeticOrder = (resultsData) => resultsData.sort((character1, character2) => {
   return (character1.name < character2.name) ? -1 : 1; //algoritmo de ordenamiento
-
 });
-/* aqui funcion para jalar la data por Male-eli*/
-export const maleSeccOrder = (resultsData) => {
-  const resultsMale= [ ]
+// avance de domingo 20feb
+export const alphabeticOrderBack = (resultsData) => resultsData.sort((character1, character2) => {
+  return (character1.name > character2.name) ? -1 : 1;
+});
+
+export const maleOrder = (resultsData) => {
+  const resultsMale = []
   for (let i = 0; i < resultsData.length; i++) {   //recorrer lista de datos
     if (resultsData[i].gender == "Male") {//evaluando parametro resultsdata y para indicar la posicion detro del array , gender es lo que vamos a comparar
       /*console.log(resultsData[i].name);*/
       resultsMale.push(resultsData[i]); //variable donde se guardan los resultados de male
-    } 
-  
+    }
+
   }
   return resultsMale;
 }
-export const maleInfo = (results) => {
-  return ( results.filter(element => element.gender == "Female")) 
 
- }
+export const femaleOrder = (resultsData) => {
+  const resultsFemale = []
+  for (let i = 0; i < resultsData.length; i++) {
+    if (resultsData[i].gender == "Female") {
+      resultsFemale.push(resultsData[i]);
+    }
+
+  }
+  return resultsFemale;
+}
+
+export const speciesOrder = (resultsData, specie) => {
+  const resultsSpecies = []
+  for (let i = 0; i < resultsData.length; i++) {
+    if (resultsData[i].species == specie) {
+      resultsSpecies.push(resultsData[i]);
+    }
+
+  }
+  return resultsSpecies;
+}
+/*export const speciesOrder = (resultsData) => {
+  const resultsSpecies = []
+  for (let i = 0; i < resultsData.length; i++) {
+    if (resultsData[i].results == "Species") {//evaluando parametro resultsdata y para indicar la posicion detro del array , gender es lo que vamos a comparar
+      /*console.log(resultsData[i].name);
+      resultsMale.push(resultsData[i]); //variable donde se guardan los resultados de male
+    }
+
+  }
+  return resultsMale;
+}*/
+
+
+
+// status personaje// estos que es Ari???
+
+/*export const characterStatus = (data) => {
+  let alive = 0;
+  let dead = 0;
+  let unknown = 0;
+
+  data.forEach((character) => {
+  switch (character.status) {
+    case 'Alive':
+      alive += 1;
+    break;
+  case 'Dead':
+      dead += 1;
+    break;
+  case 'unknown':
+      unknown += 1;
+}
+});
+return [ alive, dead, unknown ]
+}*/
+
+
--- src/main.js
+++ src/main.js
@@ -1,42 +1,112 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-// import data from './data/pokemon/pokemon.js';
 import data from './data/rickandmorty/rickandmorty.js';
+import generatorHtml from "./generatorHtml.js"
+import { alphabeticOrder } from "./data.js";
+import { alphabeticOrderBack } from "./data.js";
+import { maleOrder } from "./data.js";
+import { femaleOrder } from "./data.js";
+import { speciesOrder } from "./data.js";
+//import { Episode } from "./data.js";
 
-/*console.log(example, data);*/
+//let order = document.getElementById("order")
+const resultsData = data.results;
 
-/* funcion jalar seccion Male*/
-import { alphabeticOrder, maleSeccOrder, maleInfo } from './data.js';
-
-let resultsData = data.results;
-/*console.log(resultsData[1].name)*/
-alphabeticOrder ( resultsData )
-for (let i = 0; i <= 10; i++){ //ciclo
-    
-
-    /*console.log(resultsData [i].name)*/
-}
-/* guardar variable de Male*/
-maleSeccOrder(resultsData);//llamar a la funcion
-
-const resultsMale = [];
-
- /*resultsMale = maleSeccOrder(resultsData);//resultados se guardan en la variable*/
-
-let maleData = document.getElementById("men");
-
-let maleHtml = ``
-
-let maleFilter = maleInfo(resultsData);
-
-/*maleFilter.forEach(girls=>{
-    
-    maleHtml += '<h3>${girls.name}</h3>'
-
-    
+window.addEventListener('DOMContentLoaded', () => {
+    // for(let i=0;i < dataHTML.length; i++){
+    let allHTML = ""
+    let allCards = document.getElementById("allCards")
+    resultsData.forEach(character => {
+        allHTML += generatorHtml(character)
+    })
+    allCards.innerHTML = allHTML
+    //}
+});
+///a z
+/*order.addEventListener('change',(event)=>{
+    alphabeticOrder(dataHTML)/// mandar la data aqui
+    console.log("Recibi ...",event.target.value)
 });*/
-maleFilter.forEach(movie=>{
-    maleHtml+=`<article><h3>${movie.name}</h3></article>`
+
+/*order.addEventListener('change',(event)=>{
+    alphabeticOrderBack(dataHTML)/// mandar la data aqui
+    console.log("Recibi ...",event.target.value)
+})*/
+//pruebas del domingo se comentan lineas anteriores para que funcione el filtro A-Z, se crea un boton de prueba
+let allCards = document.getElementById("allCards")
+let buttonTest = document.getElementById("testButton");
+buttonTest.addEventListener("click", (event) => {
+    let alphabeticOrderResults = alphabeticOrder(resultsData);
+    let allHTML = ""
+    resultsData.forEach(alphabeticOrderResults => {
+        allHTML += generatorHtml(alphabeticOrderResults);
+    })
+    allCards.innerHTML = allHTML;
+});
+//boton z-a
+let testButton2 = document.getElementById("testButton2");
+testButton2.addEventListener("click", (event) => {
+    let alphabeticOrderBackResults = alphabeticOrderBack(resultsData);
+    let allHTML = ""
+    resultsData.forEach(alphabeticOrderBackResults => {
+        allHTML += generatorHtml(alphabeticOrderBackResults);
+    })
+    allCards.innerHTML = allHTML;
+});
+//boton Male
+let testButton3 = document.getElementById("testButton3");
+testButton3.addEventListener("click", (event) => {
+    let maleOrderResults = maleOrder(resultsData);
+    let allHTML = ""
+    for (let i = 0; i < maleOrderResults.length; i++) {
+        allHTML += generatorHtml(maleOrderResults[i]);
+    }
+    allCards.innerHTML = allHTML;
+});
+//boton Female
+let testButton4 = document.getElementById("testButton4");
+testButton4.addEventListener("click", (event) => {
+    let femaleOrderResults = femaleOrder(resultsData);
+    let allHTML = ""
+    for (let i = 0; i < femaleOrderResults.length; i++) {
+        allHTML += generatorHtml(femaleOrderResults[i]);
+    }
+    allCards.innerHTML = allHTML;
+});
+//boton Species
+let testButton5 = document.getElementById("testButton5");
+testButton5.addEventListener("click", (event) => {
+    let speciesOrderResults = speciesOrder(resultsData, "Robot");
+    let allHTML = ""
+    for (let i = 0; i < speciesOrderResults.length; i++) {
+        allHTML += generatorHtml(speciesOrderResults[i]);
+    }
+    allCards.innerHTML = allHTML;
+});
+
+//prueba del domingo --menu burger botton
+const menuBtn = document.querySelector(".menu-btn");
+let menuOpen = false;
+menuBtn.addEventListener("click", () => {
+    if (!menuOpen) {
+        menuBtn.classList.add("open");
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove("open");
+        menuOpen = false;
+    }
+});
+
+// prueba landingpage borrar pagina y recargarla con boton home
+
+
+let buttonInicio = document.getElementById("portalInicio")
+buttonInicio.addEventListener("click", (event) => {
+    document.getElementById("welcomePortal").innerHTML = "";
+    console.log("welcomePortal");
 })
 
-maleData.innerHTML = maleHtml;
+let buttonRegreso = document.getElementById("portalBackHome")
+buttonRegreso.addEventListener("click", (event) => {
+    location.reload();
+    console.log("portalBackHome");
+
+});
