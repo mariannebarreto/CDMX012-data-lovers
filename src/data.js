902e0563f4c77b6d446eb48c1959f14318c3d7c6
mariannebarreto <barretoink@icloud.com>
2022-02-25 17:31:55 -0600

    prueba viernes

--- src/data.js
+++ src/data.js
@@ -1,10 +1,15 @@
 //****************DECLARAMOS FUNCIONES PARA LAS SECCIONES******/
 
 //ordenado alfabético
-export const alphabeticOrder = (resultsData) => resultsData.sort((character1, character2) => {
-  return (character1.name < character2.name) ? -1 : 1; //algoritmo de ordenamiento
-});
-
+export const alphabeticOrder = (resultsData) => {
+  let testArray = [];//declaro un array vacio
+  for (let i = 0; i < resultsData.length; i++) {// recorro cadena de array
+    testArray.push(resultsData[i]);//se declara un nuevo array para que se pueda pasar todos los datos y pueda encuentrar sort es funcion de la clase array pero resultData es un argumento y sort no aplica ahi
+  }
+  return testArray.sort((character1, character2) => {
+    return (character1.name < character2.name) ? -1 : 1; //algoritmo de ordenamiento
+  });
+};
 
 //ordenado alfabético de regreso
 export const alphabeticOrderBack = (resultsData) => resultsData.sort((character1, character2) => {
@@ -69,3 +74,116 @@ export const statusCh= (resultsData) => {
   });
   return [ maleCh,femaleCh]
 }
+
+//************************ejemplo test unitarios********************//
+
+import { it } from 'eslint/lib/rule-tester/rule-tester';
+import { alphabeticOrder, alphabeticOrderBack, maleOrder, femaleOrder, speciesOrder, relevanceOrder } from '../src/data.js';
+
+let data1 = {
+  "results": [
+    {
+      "id": 1,
+      "name": "Rick Sanchez",
+      "status": "Alive",
+      "species": "Human",
+      "gender": "Male",
+    },
+    {
+      "id": 3,
+      "name": "Summer Smith",
+      "status": "Alive",
+      "species": "Human",
+      "gender": "Female",
+    },
+    {
+      "id": 441,
+      "name": "Allen Mexican Armada",
+      "status": "unknown",
+      "species": "Alien",
+      "gender": "Male",
+    },
+    {
+      "id": 161,
+      "name": "Hydrogen-F",
+      "status": "Alive",
+      "species": "Alien",
+      "gender": "female",
+    }
+
+  ]
+};
+
+let data2 = {
+  "results": [
+    {
+      "id": 441,
+      "name": "Allen Mexican Armada",
+      "status": "unknown",
+      "species": "Alien",
+      "gender": "Male",
+    },
+    {
+      "id": 161,
+      "name": "Hydrogen-F",
+      "status": "Alive",
+      "species": "Alien",
+      "gender": "female",
+    },
+    {
+      "id": 1,
+      "name": "Rick Sanchez",
+      "status": "Alive",
+      "species": "Human",
+      "gender": "Male",
+    },
+    {
+      "id": 3,
+      "name": "Summer Smith",
+      "status": "Alive",
+      "species": "Human",
+      "gender": "Female",
+    }
+
+  ]
+};
+
+describe('alphabeticOrder', () => {
+  it('is a function', () => {
+    expect(typeof alphabeticOrder).toBe('function');
+  });
+  it("return array order from A-Z", () => {
+    expect(alphabeticOrder(data1.results)).toEqual(data2.results);
+  });
+});
+
+describe('alphabeticOrderBack', () => {
+  it('is a function', () => {
+    expect(typeof alphabeticOrderBack).toBe('function');
+  });
+
+});
+
+describe('maleOrder', () => {
+  it('is a function', () => {
+    expect(typeof maleOrder).toBe('function');
+  });
+
+});
+describe('femaleOrder', () => {
+  it('is a function', () => {
+    expect(typeof femaleOrder).toBe('function');
+  });
+
+});
+describe('speciesOrder', () => {
+  it('is a function', () => {
+    expect(typeof speciesOrder).toBe('function');
+  });
+
+});
+describe('relevanceOrder', () => {
+  it('is a function', () => {
+    expect(typeof relevanceOrder).toBe('function');
+  });
+}); 
